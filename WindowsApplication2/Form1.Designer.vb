8ea601a0ba008afa910763052ed7db744113875f
pedrick16 <pedrickantazo@gmail.com>
2024-08-02 14:57:52 +0800

    First project commit in report VB

---
+++ WindowsApplication2/Form1.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ReportViewer1 = New Microsoft.Reporting.WinForms.ReportViewer()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'ReportViewer1
+        '
+        Me.ReportViewer1.Location = New System.Drawing.Point(12, 151)
+        Me.ReportViewer1.Name = "ReportViewer1"
+        Me.ReportViewer1.ServerReport.BearerToken = Nothing
+        Me.ReportViewer1.ServerReport.ReportServerUrl = New System.Uri("", System.UriKind.Relative)
+        Me.ReportViewer1.Size = New System.Drawing.Size(775, 341)
+        Me.ReportViewer1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(285, 57)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(177, 58)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(799, 519)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ReportViewer1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents ReportViewer1 As Microsoft.Reporting.WinForms.ReportViewer
+    Friend WithEvents Button1 As Button
+End Class
---
+++ WindowsApplication2/Form1.vb
@@ -0,0 +1,74 @@
+﻿Imports System.Data.SqlClient
+Imports Microsoft.Reporting.WinForms
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Me.ReportViewer1.RefreshReport()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim reportPath As String = System.IO.Path.Combine(Application.StartupPath, "Sales Orders.rdl")
+
+        ' Set up the ReportViewer control.
+        ReportViewer1.ProcessingMode = ProcessingMode.Local
+        ReportViewer1.LocalReport.ReportPath = reportPath
+
+        ' Define the data source
+        Dim dataSource1 As New ReportDataSource("DataSet1", GetData())
+
+
+        ' Clear existing data sources (optional, depending on your needs)
+        ReportViewer1.LocalReport.DataSources.Clear()
+
+
+        ' Add the new data source
+        ReportViewer1.LocalReport.DataSources.Add(dataSource1)
+
+
+
+
+        Dim reportParameters As New List(Of ReportParameter)()
+
+        ' Add parameters to the list. Replace "ParameterName" with the actual name of the parameter
+        ' defined in your report, and provide the value you want to pass.
+        reportParameters.Add(New ReportParameter("site", "PI-SP"))
+        reportParameters.Add(New ReportParameter("empnum", "123"))
+
+        ' Set the parameters to the report.
+        ReportViewer1.LocalReport.SetParameters(reportParameters)
+
+
+        ' Refresh the ReportViewer control to display the report.
+        ReportViewer1.RefreshReport()
+    End Sub
+
+
+    Private Function GetData() As DataTable
+        Dim connectionString As String = "Data Source = erp-svr;Initial Catalog=Pallet_Tagging;User ID=sa;Password=pi_dc_2011"
+        Dim query As String = "SELECT * FROM Employee"
+        Dim dataTable As New DataTable()
+
+
+
+        Using connection As New SqlConnection(connectionString)
+            Try
+                connection.Open()
+                Using command As New SqlCommand(query, connection)
+                    ' Add parameters to the SQL command
+
+                    Dim adapter As New SqlDataAdapter(command)
+
+                    ' Fill the DataTable with data from the database
+                    adapter.Fill(dataTable)
+                End Using
+            Catch ex As Exception
+                MsgBox(ex.Message)
+            Finally
+                connection.Close()
+            End Try
+
+        End Using
+
+        Return dataTable
+    End Function
+End Class
