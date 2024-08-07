690e11b1e5ead345f82767eed202b685abf9ff5e
pedrick16 <pedrickantazo@gmail.com>
2024-08-07 13:00:36 +0800

    added new form

--- WindowsApplication2/Form1.Designer.vb
+++ WindowsApplication2/Form1.Designer.vb
@@ -24,6 +24,12 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Me.ReportViewer1 = New Microsoft.Reporting.WinForms.ReportViewer()
         Me.Button1 = New System.Windows.Forms.Button()
+        Me.TXTSite = New System.Windows.Forms.TextBox()
+        Me.TXTEmp = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.FileSystemWatcher1 = New System.IO.FileSystemWatcher()
+        CType(Me.FileSystemWatcher1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'ReportViewer1
@@ -37,26 +43,74 @@ Partial Class Form1
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(285, 57)
+        Me.Button1.Location = New System.Drawing.Point(72, 97)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(177, 58)
+        Me.Button1.Size = New System.Drawing.Size(96, 24)
         Me.Button1.TabIndex = 1
         Me.Button1.Text = "Button1"
         Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TXTSite
+        '
+        Me.TXTSite.Location = New System.Drawing.Point(43, 29)
+        Me.TXTSite.Name = "TXTSite"
+        Me.TXTSite.Size = New System.Drawing.Size(150, 20)
+        Me.TXTSite.TabIndex = 2
+        '
+        'TXTEmp
+        '
+        Me.TXTEmp.Location = New System.Drawing.Point(43, 71)
+        Me.TXTEmp.Name = "TXTEmp"
+        Me.TXTEmp.Size = New System.Drawing.Size(150, 20)
+        Me.TXTEmp.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(43, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(25, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Site"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(43, 55)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(68, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Emp Number"
+        '
+        'FileSystemWatcher1
+        '
+        Me.FileSystemWatcher1.EnableRaisingEvents = True
+        Me.FileSystemWatcher1.SynchronizingObject = Me
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(799, 519)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TXTEmp)
+        Me.Controls.Add(Me.TXTSite)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.ReportViewer1)
         Me.Name = "Form1"
         Me.Text = "Form1"
+        CType(Me.FileSystemWatcher1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents ReportViewer1 As Microsoft.Reporting.WinForms.ReportViewer
     Friend WithEvents Button1 As Button
+    Friend WithEvents TXTSite As TextBox
+    Friend WithEvents TXTEmp As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents FileSystemWatcher1 As IO.FileSystemWatcher
 End Class
--- WindowsApplication2/Form1.vb
+++ WindowsApplication2/Form1.vb
@@ -7,7 +7,12 @@ Public Class Form1
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
         Dim reportPath As String = System.IO.Path.Combine(Application.StartupPath, "Sales Orders.rdl")
+
+        Dim txtSiteValue As String = TXTEmp.Text
+        Dim txtEmpValue As String = TXTSite.Text
+
 
         ' Set up the ReportViewer control.
         ReportViewer1.ProcessingMode = ProcessingMode.Local
@@ -31,8 +36,8 @@ Public Class Form1
 
         ' Add parameters to the list. Replace "ParameterName" with the actual name of the parameter
         ' defined in your report, and provide the value you want to pass.
-        reportParameters.Add(New ReportParameter("site", "PI-SP"))
-        reportParameters.Add(New ReportParameter("empnum", "123"))
+        reportParameters.Add(New ReportParameter("site", txtSiteValue))
+        reportParameters.Add(New ReportParameter("empnum", txtEmpValue))
 
         ' Set the parameters to the report.
         ReportViewer1.LocalReport.SetParameters(reportParameters)
@@ -40,22 +45,32 @@ Public Class Form1
 
         ' Refresh the ReportViewer control to display the report.
         ReportViewer1.RefreshReport()
+
+        Form2.Show()
+
     End Sub
 
 
     Private Function GetData() As DataTable
-        Dim connectionString As String = "Data Source = erp-svr;Initial Catalog=Pallet_Tagging;User ID=sa;Password=pi_dc_2011"
-        Dim query As String = "SELECT * FROM Employee"
+        ' Define the connection string
+        Dim connectionString As String = "Data Source=erp-svr;Initial Catalog=Pallet_Tagging;User ID=sa;Password=pi_dc_2011"
+
+        ' Create a new DataTable to store the data
         Dim dataTable As New DataTable()
-
-
 
         Using connection As New SqlConnection(connectionString)
             Try
                 connection.Open()
-                Using command As New SqlCommand(query, connection)
-                    ' Add parameters to the SQL command
 
+                ' Create a new SqlCommand to call the stored procedure
+                Using command As New SqlCommand("insertpallettagginguser1", connection)
+                    command.CommandType = CommandType.StoredProcedure
+
+                    ' Add parameters to the SQL command
+                    command.Parameters.AddWithValue("@Site", If(String.IsNullOrEmpty(TXTSite.Text), DBNull.Value, TXTSite.Text))
+                    command.Parameters.AddWithValue("@EmpNum", If(String.IsNullOrEmpty(TXTEmp.Text), DBNull.Value, TXTEmp.Text))
+
+                    ' Create a SqlDataAdapter to fill the DataTable
                     Dim adapter As New SqlDataAdapter(command)
 
                     ' Fill the DataTable with data from the database
@@ -66,9 +81,11 @@ Public Class Form1
             Finally
                 connection.Close()
             End Try
-
         End Using
 
+        ' Return the filled DataTable
         Return dataTable
     End Function
+
+
 End Class
